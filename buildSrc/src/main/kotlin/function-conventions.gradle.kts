77849fe07f4c1f8bef43d51b6f585d5c064419a5
Roberto Graham <14958777+RobertoGraham@users.noreply.github.com>
2025-07-06 00:23:19 +0100

    :white_check_mark: Test Function 1.

--- buildSrc/src/main/kotlin/function-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/function-conventions.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
 plugins {
     java
     id("org.springframework.boot")
@@ -16,4 +20,17 @@ repositories {
 dependencies {
     implementation(enforcedPlatform("org.springframework.cloud:spring-cloud-dependencies:2025.0.0"))
     implementation("org.springframework.cloud:spring-cloud-starter-function-webflux")
+    testImplementation(enforcedPlatform(SpringBootPlugin.BOM_COORDINATES))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    testLogging {
+        events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
---
+++ function-1/src/test/java/frankie/function1/ApplicationTests.java
@@ -0,0 +1,23 @@
+package frankie.function1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.function.Supplier;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+
+@FunctionalSpringBootTest
+final class ApplicationTests {
+
+  @Test
+  void get(@Autowired final FunctionCatalog functionCatalog) {
+    assertThat(functionCatalog.getNames(Supplier.class))
+        .singleElement()
+        .extracting(functionCatalog::lookup)
+        .extracting(Supplier.class::cast)
+        .extracting(Supplier::get)
+        .isEqualTo("Hi!");
+  }
+}
